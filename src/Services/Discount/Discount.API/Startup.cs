dea2b5661f1635707ba0b833d51343ffd0ca7a0d
Dragan Milovac <draganmilovac.1993@gmail.com>
2021-04-27 12:03:31 +0200

    Finish Discount controller and connection with PostgreSql

--- src/Services/Discount/Discount.API/Startup.cs
+++ src/Services/Discount/Discount.API/Startup.cs
@@ -1,3 +1,4 @@
+using Discount.API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -6,10 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace Discount.API
 {
@@ -31,6 +29,7 @@ namespace Discount.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Discount.API", Version = "v1" });
             });
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
