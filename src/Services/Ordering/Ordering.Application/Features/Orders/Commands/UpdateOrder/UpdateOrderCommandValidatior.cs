36684980a7f4876cd8294a0158ebbc9723b8cef0
Dragan Milovac <draganmilovac.1993@gmail.com>
2021-05-07 15:59:17 +0200

    Finish Update order command

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandValidatior.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandValidatior : AbstractValidator<UpdateOrderCommand>
+    {
+        public UpdateOrderCommandValidatior()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty().WithMessage("Username is required")
+                .NotNull()
+                .MaximumLength(50).WithMessage("Username must exceed 50 haracters.");
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty().WithMessage("Email is required");
+
+            RuleFor(p => p.TotalPrice)
+                .NotEmpty().WithMessage("Total price is required")
+                .GreaterThan(0).WithMessage("Total price should be greater than zero.");
+        }
+    }
+}
