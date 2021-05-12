9959ec439d66283aacfc492bdd89ec83903b1685
Dragan Milovac <draganmilovac.1993@gmail.com>
2021-05-12 09:55:34 +0200

    Initial Commit Infrastructure layer

---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepositor : RepositoryBase<Order>, IOrderRepository
+    {
+        public OrderRepositor(OrderContext dbContext) : base (dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUsername(string username)
+        {
+           return await _dbContext.Orders.Where(o => o.UserName == username).ToListAsync();         
+        }
+    }
+}
