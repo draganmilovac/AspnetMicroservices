4c91a36e9927f1aa02fcc257677069a7ff353cc0
Dragan Milovac <draganmilovac.1993@gmail.com>
2021-05-12 16:16:15 +0200

    Add Sql Server  image to dockercompose file and add entity framework

--- src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -9,7 +9,7 @@ namespace Ordering.Application
 {
     public static class ApplicationServiceRegistration
     {
-        public static IServiceCollection AddApplication(this IServiceCollection services)
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
